68eb1eed6c4d215db5eeb21f8139959224456df9
thilo jeremias <thilo@nispuk.com>
2021-11-03 21:52:30 +0100

    Cleaned up

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SceneKit
 import AppKit
 import os
 import RealityKit
@@ -35,81 +34,10 @@ enum ViewDetails: String, CaseIterable {
         
 }
 
-struct ConverterView: View {
-//     var model: URL?
-//    var inputFolder: URL?
-//     var progress: Double?
-//     var details: ViewDetails
-    @ObservedObject var converter: Conv2
-    
-    
-    var scene: SCNScene? {
-        //SCNScene(named: "MyScene.scnassets/Data5.usdz")
-        
-        if let fileURL = converter.model {
-            return try? SCNScene(url: fileURL)
-        }
-        return nil
-        
-            
-    }
-    var cameraNode: SCNNode? {
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
-        return cameraNode
-    }
-     
-
-
-    var body: some View {
-        if let s = scene {
-            Text("Here")
-            SceneView(
-                scene: s,
-                pointOfView: cameraNode,
-                options: [
-                    .allowsCameraControl,
-                    .autoenablesDefaultLighting,
-                    .temporalAntialiasingEnabled
-                ]
-            )
-        } else if let f = converter.progressValue {
-            VStack {
-             ProgressView(value: f)
-                Spacer()
-            }
-        } else
-        {
-            Text("Please select a directory to convert")
-        }
-//        if let m = model {
-//            Text("File there")
-//                ProgressView(value: progress)
-//
-//       } else {
-//            Text("Nothing")
-//           SceneView(
-//               scene: scene,
-//               pointOfView: cameraNode,
-//               options: [
-//                   .allowsCameraControl,
-//                   .autoenablesDefaultLighting,
-//                   .temporalAntialiasingEnabled
-//               ]
-//           )
-//
-//        }
-        
-    }
-}
-
 struct ContentView: View {
     @State var filename = "Filename"
-//    @State var model: URL?
     @State var showFileChooser = false
-    @StateObject var converter = Conv2()
-//    @State private var details : ViewDetails = .preview
+    @StateObject var converter = Converter()
     
     /// Called when the the session sends a request completed message.
     func handleRequestComplete(request: PhotogrammetrySession.Request,
@@ -123,18 +51,11 @@ struct ContentView: View {
         }
     }
     
-//    /// Called when the sessions sends a progress update message.
-//    func handleRequestProgress(request: PhotogrammetrySession.Request,
-//                                              fractionComplete: Double) {
-//        logger.log("Progress(request = \(String(describing: request)) = \(fractionComplete)")
-//    }
-
 
     var body: some View {
         VStack {
             HStack {
-//                ProgressView(value: converter?.progressValue )
-                 Text(filename)
+                Text(filename)
                 Picker(selection: $converter.detail, label: Text("Detail")) {
                                             ForEach(ViewDetails.allCases, id: \.self) { element in
                                                 Text(element.rawValue.capitalized)
@@ -150,18 +71,10 @@ struct ContentView: View {
                     panel.canChooseDirectories = true
                     if panel.runModal() == .OK {
                         self.filename = panel.url?.lastPathComponent ?? "<none>"
-//                        let outputFilename = "testing.usdz"
-//                        let outputUrl = URL(fileURLWithPath: outputFilename)
-
-//                        self.model = outputUrl
-                       // self.fileURL = panel.url!
-                        converter.fileURL = panel.url! //  = Conv2(input: panel.url!,detail: details)
-    //                    converter.run(inputFolderUrl: panel.url!)
-                    }
+                        converter.fileURL = panel.url!                   }
               }
             }
-            ConverterView(converter: converter) // model: $model,details: $details)
-
+            ConverterView(converter: converter)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     
--- Shared/Converter.swift
+++ Shared/Converter.swift
@@ -7,40 +7,33 @@
 
 import Foundation
 import RealityKit
+
 import os
-import SwiftUI
  let logger = Logger(subsystem: "com.apple.sample.photogrammetry",
                             category: "HelloPhotogrammetry")
-//@MainActor
+
 typealias Request = PhotogrammetrySession.Request
 
-//enum ViewDetails: String, CaseIterable {
-//    case preview
-//    case reduced
-//    case medium
-//    case full
-//    var det: Request.Detail {
-//        switch self {
-//        case .preview: return PhotogrammetrySession.Request.Detail.preview
-//        case .reduced: return PhotogrammetrySession.Request.Detail.reduced
-//        case .medium: return PhotogrammetrySession.Request.Detail.medium
-//        case .full: return PhotogrammetrySession.Request.Detail.full
-//    }
-//        
-//}
-
-class Conv2: ObservableObject {
+class Converter: ObservableObject {
     private var inputURL : URL?
     @Published var model: URL?
     @Published var detail: ViewDetails = .preview
-//    var session: PhotogrammetrySession?
+
     @Published var progressValue : Double?
     private var session : PhotogrammetrySession?
-    //private typealias Request = PhotogrammetrySession.Request
+    
     @MainActor
-    private func outputReady(out: URL){
-        model = out
+    private func outputReady(request: PhotogrammetrySession.Request,
+                              result: PhotogrammetrySession.Result) {
+        
+            switch result {
+                case .modelFile(let url):
+                    model = url
+                default:
+                    logger.warning("\tUnexpected result: \(String(describing: result))")
+            }
     }
+
     @MainActor
     private func progress(value:Double){
         progressValue = value
@@ -77,14 +70,9 @@ class Conv2: ObservableObject {
                                         logger.error("Request \(String(describing: request)) had an error: \(String(describing: error))")
                                        
                                     case .requestComplete(let request, let result):
-        //                                self.fileURL = outputUrl
-                                        await self.outputReady(out: outputUrl)
-                                        print(result)
-                                      case .requestProgress(let request, let fractionComplete):
-        //                                self.progressValue = fractionComplete
-                                          await self.progress(value: fractionComplete)
-        //                            ContentView.handleRequestProgress(request: request,fractionComplete: fractionComplete)
-        //                                print(fractionComplete)
+                                    await self.outputReady(request: request,result: result)
+                                    case .requestProgress(_, let fractionComplete):
+                                        await self.progress(value: fractionComplete)
                                     case .inputComplete:  // data ingestion only!
                                         logger.log("Data ingestion is complete.  Beginning processing...")
                                     case .invalidSample(let id, let reason):
@@ -116,69 +104,6 @@ class Conv2: ObservableObject {
             }
         }
     
-//= nil
-//    func run(input: URL, detail: ViewDetails)
     
 }
 
-struct Converter {
-    
-    var fileURL : URL? // = Bundle.main.url(forResource: "MyScene.scnassets/Data5", withExtension: "usdz")!
-    //URL(bundle:"/Users/thilo/Documents/xcode/test3ds/Shared/MyScene.scnassets/Data5.usdz")
-    var maybeSession: PhotogrammetrySession? = nil
-    var output :URL? = nil
-    private var outputUrl = URL(fileURLWithPath: "output.usdz") // fileURLWithPath: outputFilename)
-    var progressValue : Double = 0.0
-    var detail: Request.Detail? = .preview
-
-
-    func run(inputFolderUrl : URL) {
-        var maybeSession: PhotogrammetrySession? = nil
-        do {
-            maybeSession = try PhotogrammetrySession(input: inputFolderUrl
-            )
-            logger.log("Successfully created session.")
-        } catch {
-            logger.error("Error creating session: \(String(describing: error))")
-        }
-        guard let session = maybeSession else {
-            Foundation.exit(1)
-        }
-        
-        let waiter = Task {
-            do {
-                for try await output in session.outputs {
-                    switch output {
-                        case .processingComplete:
-                            logger.log("Processing is complete!")
-                         case .requestError(let request, let error):
-                            print(error)
-                            logger.error("Request \(String(describing: request)) had an error: \(String(describing: error))")
-                           
-                        case .requestComplete(let request, let result):
-                            print(result)
-                          case .requestProgress(let request, let fractionComplete):
-                            print(fractionComplete)
-                        case .inputComplete:  // data ingestion only!
-                            logger.log("Data ingestion is complete.  Beginning processing...")
-                        case .invalidSample(let id, let reason):
-                            logger.warning("Invalid Sample! id=\(id)  reason=\"\(reason)\"")
-                        case .skippedSample(let id):
-                            logger.warning("Sample id=\(id) was skipped by processing.")
-                        case .automaticDownsampling:
-                            logger.warning("Automatic downsampling was applied!")
-                        case .processingCancelled:
-                            logger.warning("Processing was cancelled.")
-                        @unknown default:
-                            logger.error("Output: unhandled message: \(output.localizedDescription)")
-
-                    }
-                }
-            } catch {
-                logger.error("Output: ERROR = \(String(describing: error))")
-                Foundation.exit(0)
-            }
-        }
-        
-        }
-}
