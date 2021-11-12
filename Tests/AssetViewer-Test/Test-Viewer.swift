97a6f9ed8defaca04f162799608d32232052aadd
thilo jeremias <thilo@nispuk.com>
2021-11-12 09:53:09 +0100

    Add asset reference o viewer

--- Tests/AssetViewer-Test/Test-Viewer.swift
+++ Tests/AssetViewer-Test/Test-Viewer.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct testMovieApp: App {
+    let url = Bundle.main.url(forResource: "IMG_1537", withExtension: "MOV")!
+    @ObservedObject var images: AllImages = AllImages( )
     var body: some Scene {
         WindowGroup {
-            MovieViewer()
+            MovieViewer(Movie: images, toggle: false)
+                .onAppear(perform: {images.fileURL=url})
         }
     }
 }
