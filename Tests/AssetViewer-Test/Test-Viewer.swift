4be79184cd0e4e5bd1e5648e1ff9caf4214915ff
thilo jeremias <thilo@nispuk.com>
2021-11-12 09:19:26 +0100

    Added Asset test viewer

---
+++ Tests/AssetViewer-Test/Test-Viewer.swift
@@ -0,0 +1,17 @@
+//
+//  testMovieApp.swift
+//  testMovie
+//
+//  Created by Thilo Jeremias on 10.11.21.
+//
+
+import SwiftUI
+
+@main
+struct testMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieViewer()
+        }
+    }
+}
---
+++ Tests/Lazyview-test/testmoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testmoviesApp.swift
+//  Shared
+//
+//  Created by Thilo Jeremias on 11.11.21.
+//
+
+import SwiftUI
+
+@main
+struct testmoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
